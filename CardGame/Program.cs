2b2d524c880159911187b702fe78a21c3689344a
tugberkugurlu <tugberkugurlu@gmail.com>
2015-01-26 23:53:36 +0000

    second commit

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -14,6 +14,8 @@ namespace CardGame
         }
     }
 
+    // http://www.wikihow.com/Play-Egyptian-Rat-Screw
+
     /// <summary>
     /// </summary>
     /// <remarks>
@@ -49,6 +51,17 @@ namespace CardGame
 
     public class GameState
     {
+        private readonly IEnumerable<Card> _centralPile;
+
+        public GameState(IEnumerable<Card> centralPile)
+        {
+            if (centralPile == null)
+            {
+                throw new ArgumentNullException("centralPile");
+            }
+
+            _centralPile = centralPile;
+        }
     }
 
     public class GameMove
@@ -57,23 +70,44 @@ namespace CardGame
 
     public class PlayerState
     {
+        private readonly IEnumerable<Card> _faceDownCards;
+        private readonly bool _isDealer;
+
+        public PlayerState(IEnumerable<Card> faceDownCards, bool isDealer)
+        {
+            if (faceDownCards == null)
+            {
+                throw new ArgumentNullException("faceDownCards");
+            }
+
+            _faceDownCards = faceDownCards;
+            _isDealer = isDealer;
+        }
     }
 
     public class Player
     {
+        private readonly byte _order;
         private readonly PlayerState _state;
 
-        public Player(PlayerState state)
+        public Player(byte order, PlayerState state)
         {
             if (state == null)
             {
                 throw new ArgumentNullException("state");
             }
 
+            _order = order;
             _state = state;
         }
     }
 
+    /// <summary>
+    /// A card from the deck of 52 playing cards.
+    /// </summary>
+    /// <remarks>
+    /// For more information and complete list with photos, refer to <seealso cref="http://en.wikipedia.org/wiki/Standard_52-card_deck" />.
+    /// </remarks>
     public class Card
     {
         private readonly Suit _suit;
@@ -147,139 +181,5 @@ namespace CardGame
         Jack,
         Queen,
         King
-    }
-
-    /// <summary>
-    /// List of the deck of 52 playing cards.
-    /// </summary>
-    /// <remarks>
-    /// For more information and complete list with photos, refer to <seealso cref="http://en.wikipedia.org/wiki/Standard_52-card_deck" />.
-    /// </remarks>
-    public enum Cards
-    {
-        ClubsAce = 101,
-        ClubsTwo = 102,
-        ClubsThree = 103,
-        ClubsFour = 104,
-        ClubsFive = 105,
-        ClubsSix = 106,
-        ClubsSeven = 107,
-        ClubsEight = 108,
-        ClubsNine = 109,
-        ClubsTen = 110,
-        ClubsJack = 111,
-        ClubsQueen = 112,
-        ClubsKing = 113,
-
-        DiamondsAce = 201,
-        DiamondsTwo = 202,
-        DiamondsThree = 203,
-        DiamondsFour = 204,
-        DiamondsFive = 205,
-        DiamondsSix = 206,
-        DiamondsSeven = 207,
-        DiamondsEight = 208,
-        DiamondsNine = 209,
-        DiamondsTen = 210,
-        DiamondsJack = 211,
-        DiamondsQueen = 212,
-        DiamondsKing = 213,
-
-        HeartsAce = 301,
-        HeartsTwo = 302,
-        HeartsThree = 303,
-        HeartsFour = 304,
-        HeartsFive = 305,
-        HeartsSix = 306,
-        HeartsSeven = 307,
-        HeartsEight = 308,
-        HeartsNine = 309,
-        HeartsTen = 310,
-        HeartsJack = 311,
-        HeartsQueen = 312,
-        HeartsKing = 313,
-
-        SpadesAce = 301,
-        SpadesTwo = 302,
-        SpadesThree = 303,
-        SpadesFour = 304,
-        SpadesFive = 305,
-        SpadesSix = 306,
-        SpadesSeven = 307,
-        SpadesEight = 308,
-        SpadesNine = 309,
-        SpadesTen = 310,
-        SpadesJack = 311,
-        SpadesQueen = 312,
-        SpadesKing = 313
-    }
-
-    public class CardMappings
-    {
-        private static readonly IDictionary<Cards, string> CardMappingDictionary = new Dictionary<Cards, string>
-        {
-            { Cards.ClubsAce, "" },
-            { Cards.ClubsTwo, "" },
-            { Cards.ClubsThree, "" },
-            { Cards.ClubsFour, "" },
-            { Cards.ClubsFive, "" },
-            { Cards.ClubsSix, "" },
-            { Cards.ClubsSeven, "" },
-            { Cards.ClubsNine, "" },
-            { Cards.ClubsTen, "" },
-            { Cards.ClubsJack, "" },
-            { Cards.ClubsQueen, "" },
-            { Cards.ClubsKing, "" },
-
-            { Cards.DiamondsAce, "" },
-            { Cards.DiamondsTwo, "" },
-            { Cards.DiamondsThree, "" },
-            { Cards.DiamondsFour, "" },
-            { Cards.DiamondsFive, "" },
-            { Cards.DiamondsSix, "" },
-            { Cards.DiamondsSeven, "" },
-            { Cards.DiamondsNine, "" },
-            { Cards.DiamondsTen, "" },
-            { Cards.DiamondsJack, "" },
-            { Cards.DiamondsQueen, "" },
-            { Cards.DiamondsKing, "" },
-
-            { Cards.HeartsAce, "" },
-            { Cards.HeartsTwo, "" },
-            { Cards.HeartsThree, "" },
-            { Cards.HeartsFour, "" },
-            { Cards.HeartsFive, "" },
-            { Cards.HeartsSix, "" },
-            { Cards.HeartsSeven, "" },
-            { Cards.HeartsNine, "" },
-            { Cards.HeartsTen, "" },
-            { Cards.HeartsJack, "" },
-            { Cards.HeartsQueen, "" },
-            { Cards.HeartsKing, "" },
-
-            { Cards.SpadesAce, "" },
-            { Cards.SpadesTwo, "" },
-            { Cards.SpadesThree, "" },
-            { Cards.SpadesFour, "" },
-            { Cards.SpadesFive, "" },
-            { Cards.SpadesSix, "" },
-            { Cards.SpadesSeven, "" },
-            { Cards.SpadesNine, "" },
-            { Cards.SpadesTen, "" },
-            { Cards.SpadesJack, "" },
-            { Cards.SpadesQueen, "" },
-            { Cards.SpadesKing, "" }
-        };
-
-        private CardMappings()
-        {
-        }
-
-        public string this[Cards i]
-        {
-            get
-            {
-            }
-        }
     }
 }
